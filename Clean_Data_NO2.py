f6487a196ddca5dc5eca923def3d57fad037a7a7
AnneBai0802 <93656196+AnneBai0802@users.noreply.github.com>
2023-03-01 21:46:36 +0800

    Add files via upload

--- Clean_Data_NO2.py
+++ Clean_Data_NO2.py
@@ -8,6 +8,7 @@ Created on Thu Feb 23 02:55:54 2023
 import pandas as pd
 import numpy as np
 import math
+import Site_To_COde
 
 
 def clean_NO2():
@@ -20,24 +21,43 @@ def clean_NO2():
         DESCRIPTION. gives the average NO2 emission of each borough over the year
 
     '''
+    
+    areas,codes=Site_To_COde.STC()
     #read data from file
-    data=pd.read_csv('NO2_london_boroughs.csv')
+    data=pd.read_csv('data_2015_2023.csv')
     
     #divide into boroughs
-    NO2=np.array([0.0 for i in range(32)])
-    for i in range(32):
+    NO2=np.array([0.0 for i in range(len(areas))])
+    start=0
+    length=len(data)
+    for i in range(len(areas)):
         total=0
         count=0
-        records=data[data['Site']==i+1]
-        
+        records=[]
+        print(codes[i])
+        if areas[i]=='Hackney and City of London':
+            
+            for j in range(0,length):
+                current=data.iloc[j]
+                if current['code'] in codes[i]:
+                    records.append(current['no2'])
+        else:
+            for j in range(start,length):
+                current=data.iloc[j]
+                if current['code'] in codes[i]:
+                    records.append(current['no2'])
+                    start=j
+                elif len(records)!=0:
+                    start=j
+                    break
+
         #calculate average NO2 level over 2021
         for j in range(len(records)):
-            value=records.iloc[j]['Value']
+            value=records[j]
             if not math.isnan(value):
                 total+=value
                 count+=1
-            else:
-                total+=0
+            
         
         if count==0:
             Average_NO2=0
@@ -47,7 +67,8 @@ def clean_NO2():
             NO2[i]=Average_NO2
         except:
             NO2[i]=0
+        print(NO2)
         
         
-    return NO2
-    
+    return areas,NO2
+   
--- Core.py
+++ Core.py
@@ -13,17 +13,18 @@ import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 from operator import itemgetter
 
-NO2=Clean_Data_NO2.clean_NO2()
+areas,NO2=Clean_Data_NO2.clean_NO2()
 Name_Boroughs,Percentage_Death_Boroughs=Clean_Data_Covid.clean_Covid()
 
 #sort the values in terms of NO2 level
 matrix=[]
 for i in range(len(NO2)):
-    matrix.append([NO2[i],Percentage_Death_Boroughs[i],Name_Boroughs[i]])
+    if NO2[i]!=0:
+        matrix.append([NO2[i],Percentage_Death_Boroughs[i],Name_Boroughs[i]])
 matrix=sorted(matrix, key=itemgetter(0))
-NO2=np.array([matrix[i][0] for i in range(len(matrix))])
-Percentage_Death_Boroughs=np.array([matrix[i][1] for i in range(len(matrix))])
-Name_Boroughs=np.array([matrix[i][2] for i in range(len(matrix))])
+NO2=np.array([matrix[i][0] for i in range(5,len(matrix))])
+Percentage_Death_Boroughs=np.array([matrix[i][1] for i in range(5,len(matrix))])
+Name_Boroughs=np.array([matrix[i][2] for i in range(5,len(matrix))])
 
 po,po_cov=curve_fit(linear,NO2,Percentage_Death_Boroughs)
 plt.plot(NO2,Percentage_Death_Boroughs,'x',label='Recorded Data')
@@ -31,4 +32,4 @@ plt.plot(NO2,linear(NO2,po[0],po[1]),'-',label='Simulated line, gradient='+str(r
 plt.xlabel('Average $NO_{2}$ /$\mu g m^{-3}$')
 plt.ylabel('Death per 1000 people')
 plt.legend()
-plt.savefig('Percentage Death against NO2 in 2021',dpi=500)
+plt.savefig('Percentage Death 2021 against NO2 in 2018-19',dpi=500)
